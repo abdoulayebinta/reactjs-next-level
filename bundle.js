2e2978c0f812f60d7eba14af58170b02a1eef548
abdoulayebinta <bahabdoulayebinta@gmail.com>
2019-08-01 22:55:36 +0000

    Add babel config

---
+++ bundle.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _path = _interopRequireDefault(require("path"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var log = function log(string) {
+  console.log("Hello World");
+};
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log(`Hello World`);
+import path from 'path';
+
+const log = (string) => {
+    console.log(`Hello World`);
+};
